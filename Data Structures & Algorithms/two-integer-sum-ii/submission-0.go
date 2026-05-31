2785bb734bd0b26631eb36926ff6398db2e022da
Kamal Joshi <kamal2311@gmail.com>
2026-05-31 14:54:35 -0500

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.go
@@ -0,0 +1,15 @@
+func twoSum(numbers []int, target int) []int {
+
+	for i, j := 0, len(numbers)-1; i < j; {
+		if numbers[i] + numbers[j] == target {
+			return []int{i+1,j+1}
+		} else if numbers[i] + numbers[j] < target {
+			i++
+		} else {
+			j--
+		}
+	} 
+
+	return nil
+
+}
